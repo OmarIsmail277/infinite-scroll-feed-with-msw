7876de80c4831646cb9d39f6c46aa97f162c24eb
OmarIsmail277 <omar.ismail277@gmail.com>
2025-11-29 12:54:18 +0200

    Create useProducts hook with React Query infinite query

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,21 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+import type { ProductsResponse } from "../types/products";
+
+const fetchProducts = async (page: number): Promise<ProductsResponse> => {
+  const response = await axios.get<ProductsResponse>(
+    `/api/products?page=?${page}`
+  );
+  return response.data;
+};
+
+export const useProducts = () => {
+  return useInfiniteQuery({
+    queryKey: ["products"],
+    queryFn: ({ pageParam = 0 }) => fetchProducts(pageParam),
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+    initialPageParam: 0,
+    staleTime: 5 * 60 * 1000, // 5 mins
+  });
+};
